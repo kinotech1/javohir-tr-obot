f5428c766759881409ec1c4275cf6c83b69cc2ee
kinotech1 <96439913+kinotech1@users.noreply.github.com>
2021-12-28 18:02:38 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,12 @@ from googletrans import Translator
 updater = Updater('5039566479:AAGINHjFdCE1Y7bjRoulTynUYkGgDx7pxcY',use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('hi iam google translater ')
+ updater.message.reply_text('Salom, Botimizga xush kelibsiz! Tarjima qiladigan sozingizni menga jonating! ')
  
 def echo(updater,context):
  usr_msg =updater.message.text
  translator = Translator()
- translation = translator.translate(usr_msg,dest='hi') 
+ translation = translator.translate(usr_msg,dest='uz') 
  updater.message.reply_text(translation.text)
  
 dp =updater.dispatcher.add_handler
